959970db78538282db6047f21daf813ed842caa4
James <jrh-dev@protonmail.com>
2022-06-20 11:09:02 +0100

    some ammendments

--- day_01/day_01.R
+++ day_01/day_01.R
@@ -1,13 +1,10 @@
-input_1 <- as.numeric(readLines("day_1/data/day_1.txt"))
 
-# Part 1 
+input_1 <- as.numeric(readLines("day_1/day_1.txt"))
+
+# Part 1 answer
 sum(diff(input_1) > 0)
 
-# Answer 1715
-
-# Part 2
 input_roll <- zoo::rollsum(input_1, 3)
 
+# Part 2 answer
 sum(diff(input_roll) > 0)
-
-# Answer 1739
